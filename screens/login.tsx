cdf479c9d4fcb38b10b1af005823f85f6bbeca13
Carlos Davi Dantas <dantasdavi@hotmail.com>
2024-11-16 14:21:10 -0300

    refatoração da tela login

---
+++ screens/login.tsx
@@ -0,0 +1,74 @@
+import { View, Text, TouchableOpacity, TextInput, Dimensions, StyleSheet } from 'react-native';
+
+export function LoginScreen() {
+    return (
+        <View style={styles.backContainer}>
+            <View style={styles.secondBackContainer}>
+                <View style={styles.thirdContainer}>
+                    <Text style={styles.titlesText}>Login</Text>
+                    <Text style={styles.texts}>Email</Text>
+                    <TextInput style={styles.input}></TextInput>
+                    <Text style={styles.texts}>Senha</Text>
+                    <TextInput style={styles.input}></TextInput>
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonsText}>ENTRAR</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    backContainer: {
+        backgroundColor: "#E2D3FF",
+        justifyContent: "center",
+        alignItems: "center",
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+    },
+    secondBackContainer: {
+        backgroundColor: "#A77BFF",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "90%",
+        height: "90%",
+        borderRadius: 10
+    },
+    thirdContainer: {
+        backgroundColor: "#FFFFFF",
+        width: "80%",
+        height: "80%",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        opacity: 0.5,
+        borderRadius: 10
+    },
+    titlesText: {
+        fontFamily: "Montserrat",
+        fontWeight: "bold",
+        fontSize: 50,
+        color: "#9847FF"
+    },
+    texts: {
+        fontSize: 20,
+    },
+    input: {
+        backgroundColor: "#F5F5F5",
+        width: "80%",
+        height: "7%"
+    },
+    button: {
+        backgroundColor: "#9847FF",
+        width: "60%",
+        height: "7%",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10
+    },
+    buttonsText: {
+        fontSize: 30,
+        fontWeight: "bold"
+    }
+
+})
